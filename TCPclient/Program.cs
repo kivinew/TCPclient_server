9fa411227b41d369844d8d08c25ba7aa6cae8f8d
Иван Кудрявцев <kivinew@gmail.com>
2017-03-18 00:00:21 +0700

    I finally could to sent my TCP client-server program to GitHub) Ughh...

---
+++ TCPclient/Program.cs
@@ -0,0 +1,61 @@
+﻿// Это программа клиента
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "<<TcpClient>>";
+            TcpClient client = null;
+            int clientCount = 0, connectFlag = 0;                       // флаг подключения
+            while (client == null)
+            {
+                clientCount++;
+                try
+                {
+                    client = new TcpClient("127.0.0.1", 12345);
+                    connectFlag = 1;                                    // флаг подключения
+                    Console.WriteLine("Connected! Let's send a message!");
+                    NetworkStream stream = client.GetStream();
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes($"Hello, it's №{clientCount} message from client");
+                    byte[] receiveBuffer = new byte[client.ReceiveBufferSize];
+                    stream.Write(sendBuffer, 0, sendBuffer.Length);
+                    Console.WriteLine("Message sent successfully!");
+                    if (stream.CanRead)                                 // возможность чтения из потока
+                    {
+                        StringBuilder message = new StringBuilder();
+                        int bytes = 0;
+                        do
+                        {
+                            bytes = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+                            message.AppendFormat($"{0}" + Encoding.UTF8.GetString(receiveBuffer, 0, bytes));
+                        } while (stream.DataAvailable);                 // доступность данных для чтения
+                        Console.WriteLine($"Received message:\n{message}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I cannot read at this time :(");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (connectFlag == 1)
+                        Console.WriteLine("Disconnected");
+                    Console.WriteLine($"{clientCount} попытка:\n{ex.Message}\n");
+                }
+                finally
+                {
+                    Console.WriteLine("-------------------------------------------------------------\n\n");
+                    if(client!=null)
+                        client.Close();
+                }
+            }
+            Console.WriteLine("Good bye!");
+            Console.Read();
+        }
+    }
+}
---
+++ TCPserver/Program.cs
@@ -0,0 +1,61 @@
+﻿// Это программа сервера
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "<<TcpServer>>";
+            int bytes = 0;                                              // количество принятых байт
+            int connectFlag = 0;                                        // отметка о подключении
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");     // локальный адрес сервера
+                server = new TcpListener(localAddr, 12345);             // сервер 
+                server.Start();
+                do
+                {
+                    var client = server.AcceptTcpClient();              // входящее подключение
+                    connectFlag = 1;
+                    Console.WriteLine("Client connected.");
+                    NetworkStream stream = client.GetStream();          // поток чтения/записи клиента
+                    byte[] receiveBuffer = new byte[client.ReceiveBufferSize];
+                    if (stream.CanRead)                                 // возможность чтения из потока
+                    {
+                        StringBuilder receivedMessage = new StringBuilder();
+                        do
+                        {
+                            bytes = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+                            receivedMessage.AppendFormat($"{0}" + Encoding.UTF8.GetString(receiveBuffer, 0, bytes));
+                        } while (stream.DataAvailable);                 // доступность данных для чтения
+                        Console.WriteLine($"Получено сообщение от клиента:\n{receivedMessage}");
+                    }
+                    string message = "Ваше сообщение получено"+ bytes;   // Это тестовое сообщение от сервера
+                    byte[] data = Encoding.UTF8.GetBytes(message);      // преобразуем сообщение в массив байтов
+                    stream.Write(data, 0, data.Length);                 // запись массива байтов в поток
+                    Console.WriteLine("Сообщение отправлено!");
+                    stream.Close();
+                    client.Close();                                     // подключение к клиенту закрыто
+                    Console.WriteLine("Disconnected");
+                } while (true);
+            }
+            catch(Exception ex)
+            {
+                if (connectFlag == 1)
+                    Console.WriteLine("...lost connection");
+                Console.WriteLine($"-------------------------------------\n{ex.Message}\n------------------------------------");
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
